4f582b0ac40a88fbf2f726d69d9329a998deaa9a
Bartosz Zasieczny <barteqx@gmail.com>
2016-06-19 23:25:45 +0200

    models pt3

---
+++ src/database/models/__init__.py
@@ -0,0 +1,10 @@
+__all__ = ["citizen", "user", "policeman", "investigation", "fine", "arrest", "cell", "object_relations"]
+
+from src.database.models.citizen import *
+from src.database.models.cell import *
+from src.database.models.arrest import *
+from src.database.models.fine import *
+from src.database.models.investigation import *
+from src.database.models.policeman import *
+from src.database.models.user import *
+from src.database.models.object_relations import *
